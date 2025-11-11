94f7f1d0012b98a8b922769a2224190b05cf575a
Mahesh8120 <jonnalagadamahi@gmail.com>
2025-11-11 18:21:15 +0530

    ansible roles

---
+++ 70-acm/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "terraform-mahesh"
+    key          = "terraform-acm"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "sitaram.icu"
+}
+
+variable "zone_id" {
+    default = "Z09532653LYSJAOMBYJTE"
+}
