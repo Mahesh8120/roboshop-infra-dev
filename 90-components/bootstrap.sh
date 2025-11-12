971fe1bcd3784df7e2889c6f4cf6b3d902314ad8
Mahesh8120 <jonnalagadamahi@gmail.com>
2025-11-12 18:56:56 +0530

    terraform

--- 90-components/bootstrap.sh
+++ 90-components/bootstrap.sh
@@ -1,28 +1,41 @@
-#!/bin/bash
+# #!/bin/bash
 
-component=$1
-environment=$2
+# component=$1
+# environment=$2
+# dnf install ansible -y
+
+# REPO_URL=https://github.com/Mahesh8120/Ansible_Roboshop-roles-tf.git
+# REPO_DIR=/opt/roboshop/ansible
+# ANSIBLE_DIR=ansible-roboshop-roles-tf
+
+# mkdir -p $REPO_DIR
+# mkdir -p /var/log/roboshop/
+# touch ansible.log
+
+# cd $REPO_DIR
+
+# # check if ansible repo is already cloned or not
+
+# if [ -d $ANSIBLE_DIR ]; then
+
+#     cd $ANSIBLE_DIR
+#     git pull
+# else
+#     git clone $REPO_URL
+#     cd $ANSIBLE_DIR
+# fi
+# echo "environment is: $2"
+# ansible-playbook -e component=$component -e env=$environment main.yaml
+
+#!/bin/bash
+set -e
+
+component="$1"
+environment="$2"
+
 dnf install ansible -y
 
-REPO_URL=https://github.com/Mahesh8120/Ansible_Roboshop-roles-tf.git
-REPO_DIR=/opt/roboshop/ansible
-ANSIBLE_DIR=ansible-roboshop-roles-tf
+echo "Running ansible-pull for $component in $environment"
 
-mkdir -p $REPO_DIR
-mkdir -p /var/log/roboshop/
-touch ansible.log
-
-cd $REPO_DIR
-
-# check if ansible repo is already cloned or not
-
-if [ -d $ANSIBLE_DIR ]; then
-
-    cd $ANSIBLE_DIR
-    git pull
-else
-    git clone $REPO_URL
-    cd $ANSIBLE_DIR
-fi
-echo "environment is: $2"
-ansible-playbook -e component=$component -e env=$environment main.yaml
+ansible-pull -U https://github.com/Mahesh8120/Ansible_Roboshop-roles-tf.git \
+  -e component="$component" -e env="$environment" main.yaml
